c0bd14e213d52477aeea4789f5a79a0287d2f3ed
Víctor Cervera <vlcervera@gmail.com>
2022-05-27 14:53:28 +0200

    First version for a command line application

--- app/main.py
+++ app/main.py
@@ -2,9 +2,16 @@ from subscriptions import Subscriptions
 from tiered_pricing import TieredPricing
 
 pricing = TieredPricing()
-for amount in range(1, 55):
-    subscriptions = Subscriptions(amount=amount)
+
+while True:
+
+    amount_of_subscriptions = int(input("Enter the number of subscriptions that you want to buy (enter 0 to exit): "))
+
+    if amount_of_subscriptions == 0:
+        break
+    subscriptions = Subscriptions(amount=amount_of_subscriptions)
     price = pricing.calculate_for(subscriptions)
     print(
-        f"""Price={price}, subscription/price = {price / subscriptions.amount} for {subscriptions} subscriptions"""
+        f"""Price={price}, subscription/price = {price / subscriptions.amount} for {subscriptions.amount} subscriptions"""
     )
+
